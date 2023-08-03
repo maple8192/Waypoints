3a746cf21070e45f9be30679edcd46e0c4e9f2fe
maple8192 <57043077+maple8192@users.noreply.github.com>
2023-08-03 20:51:32 +0900

    :sparkles: コマンド処理のためのクラスを追加 #1

--- src/main/kotlin/io/github/maple8192/waypoints/Waypoints.kt
+++ src/main/kotlin/io/github/maple8192/waypoints/Waypoints.kt
@@ -1,6 +1,16 @@
 package io.github.maple8192.waypoints
 
+import io.github.maple8192.waypoints.commands.CancelGuide
+import io.github.maple8192.waypoints.commands.SetWaypoint
+import io.github.maple8192.waypoints.commands.ShowGuide
+import io.github.maple8192.waypoints.commands.WaypointList
 import org.bukkit.plugin.java.JavaPlugin
 
 class Waypoints : JavaPlugin() {
+    override fun onEnable() {
+        getCommand("setwaypoint")?.setExecutor(SetWaypoint())
+        getCommand("waypoints")?.setExecutor(WaypointList())
+        getCommand("go")?.setExecutor(ShowGuide())
+        getCommand("cancel")?.setExecutor(CancelGuide())
+    }
 }
---
+++ src/main/kotlin/io/github/maple8192/waypoints/commands/CancelGuide.kt
@@ -0,0 +1,11 @@
+package io.github.maple8192.waypoints.commands
+
+import org.bukkit.command.Command
+import org.bukkit.command.CommandExecutor
+import org.bukkit.command.CommandSender
+
+class CancelGuide : CommandExecutor {
+    override fun onCommand(sender: CommandSender, command: Command, label: String, args: Array<out String>?): Boolean {
+        return true
+    }
+}
