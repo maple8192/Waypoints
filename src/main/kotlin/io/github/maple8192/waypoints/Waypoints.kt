33fc9b88dbb94cab54bb66faf0d879f75ebbddfc
maple8192 <57043077+maple8192@users.noreply.github.com>
2023-08-04 02:34:35 +0900

    :sparkles: 処理するためのクラスを追加 #1

--- src/main/kotlin/io/github/maple8192/waypoints/Waypoints.kt
+++ src/main/kotlin/io/github/maple8192/waypoints/Waypoints.kt
@@ -1,9 +1,6 @@
 package io.github.maple8192.waypoints
 
-import io.github.maple8192.waypoints.commands.CancelGuide
-import io.github.maple8192.waypoints.commands.SetWaypoint
-import io.github.maple8192.waypoints.commands.ShowGuide
-import io.github.maple8192.waypoints.commands.WaypointList
+import io.github.maple8192.waypoints.commands.*
 import io.github.maple8192.waypoints.timer.BossBarHandler
 import io.github.maple8192.waypoints.waypoint.WaypointHandler
 import org.bukkit.plugin.java.JavaPlugin
@@ -11,6 +8,7 @@ import org.bukkit.plugin.java.JavaPlugin
 class Waypoints : JavaPlugin() {
     override fun onEnable() {
         getCommand("setwaypoint")?.setExecutor(SetWaypoint())
+        getCommand("removewaypoint")?.setExecutor(RemoveWaypoint())
         getCommand("waypoints")?.setExecutor(WaypointList())
         getCommand("go")?.setExecutor(ShowGuide())
         getCommand("cancel")?.setExecutor(CancelGuide())
