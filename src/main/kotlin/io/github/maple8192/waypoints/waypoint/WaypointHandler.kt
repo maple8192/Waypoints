a3f0953350206c4602c3449a763f957af4305ffa
maple8192 <57043077+maple8192@users.noreply.github.com>
2023-08-04 02:00:49 +0900

    :sparkles: /goコマンドの中身を実装 #1
    ガイド表示は未実装

--- src/main/kotlin/io/github/maple8192/waypoints/waypoint/WaypointHandler.kt
+++ src/main/kotlin/io/github/maple8192/waypoints/waypoint/WaypointHandler.kt
@@ -1,10 +1,15 @@
 package io.github.maple8192.waypoints.waypoint
 
+import org.bukkit.Bukkit
 import org.bukkit.Location
+import org.bukkit.boss.BarColor
+import org.bukkit.boss.BarStyle
+import org.bukkit.boss.BossBar
 import java.util.*
 
 class WaypointHandler {
     private val waypoints: MutableMap<UUID, MutableMap<String, Location>> = mutableMapOf()
+    private val guides: MutableMap<UUID, MutableMap<String, BossBar>> = mutableMapOf()
 
     fun addWaypoint(uuid: UUID, name: String, location: Location) {
         if (!waypoints.containsKey(uuid)) {
@@ -21,5 +26,14 @@ class WaypointHandler {
     fun getWaypoints(uuid: UUID): Map<String, Location> {
         if (!waypoints.containsKey(uuid)) return mapOf()
         return waypoints[uuid]!!.toMap()
+    }
+
+    fun addGuide(uuid: UUID, name: String) {
+        if (!waypoints.containsKey(uuid)) return
+        if (waypoints[uuid]?.get(name) == null) return
+        if (guides[uuid]?.containsKey(name) == true) return
+
+        if (!guides.containsKey(uuid)) guides[uuid] = mutableMapOf()
+        guides[uuid]?.put(name, Bukkit.createBossBar("", BarColor.GREEN, BarStyle.SOLID))
     }
 }
