ca5b7e81aa8b00adac227b6c5b4f297533d7d9f8
maple8192 <57043077+maple8192@users.noreply.github.com>
2023-08-04 01:31:22 +0900

    :sparkles: /setwaypointコマンドの中身を実装 #1

--- src/main/kotlin/io/github/maple8192/waypoints/commands/SetWaypoint.kt
+++ src/main/kotlin/io/github/maple8192/waypoints/commands/SetWaypoint.kt
@@ -1,11 +1,19 @@
 package io.github.maple8192.waypoints.commands
 
+import io.github.maple8192.waypoints.Waypoints
 import org.bukkit.command.Command
 import org.bukkit.command.CommandExecutor
 import org.bukkit.command.CommandSender
+import org.bukkit.entity.Player
 
 class SetWaypoint : CommandExecutor {
     override fun onCommand(sender: CommandSender, command: Command, label: String, args: Array<out String>?): Boolean {
+        if (sender !is Player) return true
+
+        if (args?.size != 1) return true
+
+        Waypoints.handler.addWaypoint(sender.uniqueId, args[0], sender.location)
+
         return true
     }
 }
